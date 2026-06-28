625d7fed11e0ee4fe9c43c8aac6b8367b550cb76
rahul pareek <itsrahul615@gmail.com>
2026-06-28 23:25:13 +0530

    creating hospital management system

---
+++ src/main/java/com/rahulscripts/hospitalManagementSystem/entity/Patient.java
@@ -0,0 +1,36 @@
+package com.rahulscripts.hospitalManagementSystem.entity;
+
+import com.rahulscripts.hospitalManagementSystem.entity.type.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private String email;
+
+    private String gender;
+
+    @Enumerated(value = EnumType.STRING)
+    private BloodGroupType bloodGroup;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
