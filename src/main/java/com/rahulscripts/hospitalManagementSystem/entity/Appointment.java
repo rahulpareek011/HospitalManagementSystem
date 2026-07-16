42699609650c7227af989b74fd505c113f217b2f
rahul pareek <itsrahul615@gmail.com>
2026-07-16 23:55:35 +0530

    starting orphan

--- src/main/java/com/rahulscripts/hospitalManagementSystem/entity/Appointment.java
+++ src/main/java/com/rahulscripts/hospitalManagementSystem/entity/Appointment.java
@@ -20,7 +20,7 @@ public class Appointment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false)f
     private LocalDateTime appointmentTime;
 
     @Column(length = 500)
